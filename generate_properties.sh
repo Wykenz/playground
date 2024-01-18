2cac141224b68a43355b0b5e681de39c2b6c8b7a
pgydev <pgy.dev@github.com>
2024-01-18 20:20:19 +0100

    check if its in the json, get tomcat version from files, check if its in the yml

--- generate_properties.sh
+++ generate_properties.sh
@@ -4,9 +4,13 @@ function get_liferay_version_format {
 	local url=$(grep -E ".${TIME_STAMP}." bundles.yml)
 	local file_url="${url##*: }"
 	local base="${file_url%/*}"
-
 	LIFERAY_VERSION="${base##*/}"
-
+	if [[ "${LIFERAY_VERSION}" =~ ^7\.[0-9]+\.[0-9]+\.[0-9]+$ ]]
+	then
+		tag="${LIFERAY_VERSION##*.}"
+		base="${LIFERAY_VERSION%.*}"
+		LIFERAY_VERSION="${base}-sp${tag}"
+	fi
 }
 
 function generate_release_properties_file {
@@ -20,7 +24,7 @@ function generate_release_properties_file {
 		echo "git.hash.liferay-portal-ee="
 		echo "liferay.docker.fix.pack.url=${LIFERAY_DOCKER_FIX_PACK_URL}"
 		echo "liferay.docker.image=liferay/dxp:${LIFERAY_DOCKER_IMAGE}"
-		echo "liferay.docker.tags=liferay/dxp:${LIFERAY_DOCKER_IMAGE}${LIFERAY_DOCKER_TAGS}"
+		echo "liferay.docker.tags=liferay/dxp:${LIFERAY_DOCKER_IMAGE}/${LIFERAY_DOCKER_TAGS}"
 		echo "liferay.docker.test.hotfix.url=${LIFERAY_DOCKER_TEST_HOTFIX_URL}"
 		echo "liferay.docker.test.installed.patch=${LIFERAY_DOCKER_TEST_INSTALLED_PATCH}"
 		echo "liferay.product.version=${PRODUCT_VERSION}"
@@ -39,7 +43,7 @@ function get_time_stamp {
 	local version="${1}"
 
 	url="https://releases.liferay.com/dxp/${version}/"
-	filename=$(curl -s "$url" | grep -oP 'href="\K[^"?]+' | grep -vE '\?C=|;O=' | grep 'tomcat' | grep -E '\.7z$')
+	filename=$(curl -s "$url" | grep -oP 'href="\K[^"?]+' | grep -vE '\?C=|;O=' | grep -E 'tomcat' | grep -E '\.7z$|\.zip$')
 	numeric_part=${filename%.*}
 	numeric_part=${numeric_part##*-}
  	TIME_STAMP="${numeric_part}"
@@ -47,20 +51,39 @@ function get_time_stamp {
 
 }
 
+function get_value_from_files {
+	local file_name="${1}"
+	local name
+
+	if [[ "${tag}" == ".appServerTomcatVersion" ]]
+	then
+		curl -o ./"${file_name}" "https://${BUNDLE_URL}"
+		if [[ "${file_name}" == *.7z ]]
+		then
+			name=$(7z l "${file_name}" | grep -m 1 "/tomcat")
+			echo "${name##*-}"
+		else
+			name=$(unzip l "${file_name}" | grep -m 1 "/tomcat")
+			echo "${name##*-}"
+		fi
+	fi
+	rm -r "${file_name}"
+}
+
 function get_json {
 	local version="${1}"
 	local tag="${2}"
-	local url=$(grep -E ".${TIME_STAMP}." bundles.yml)
-	local file_url="${url##*: }"
-	local file_name="${file_url##*/}"
+	local name
+	local file_name="${BUNDLE_URL##*/}"
 
-	result=$(curl -s "${file_url}" | jq '.[] | select(.liferayDockerImage=="liferay/dxp:'${version}'") | '${tag}'')
+	result=$(curl -s "https://releases.liferay.com/tools/workspace/.product_info.json" | jq '.[] | select(.liferayDockerImage=="liferay/dxp:'${version}'") | '${tag}'')
 
-	if [[ -n "${result}" ]] && [[ "${tag}" == ".appServerTomcatVersion" ]]
+	if [[ -z "${result}" ]]
 	then
-		curl -o "${file_name}" ./"${file_name}"
-
-		##tomcat_folder=$(find <output_directory> -type d -name "tomcat*")
+		name=$(get_value_from_files "${file_name}")
+		echo "${name}"
+	else
+		echo "${result}"
 	fi
 
 }
@@ -70,7 +93,16 @@ function get_yml {
 	local version="${2}"
 	local tag="${3}"
 
-	yq '."'"${main_key}"'"."'"${version}"'"."'"${tag}"'"' bundles.yml
+	result=$(yq '."'"${main_key}"'"."'"${version}"'"."'"${tag}"'"' bundles.yml)
+
+	if [[ -z "${result}" ]]
+	then
+		echo ""
+	else
+		echo "${result}"
+	fi
+
+
 
 }
 
@@ -81,11 +113,11 @@ function set_value {
 
 	main_key=$(get_main_key "${LIFERAY_VERSION}")
 
-	TOMCAT_VERSION=$(get_json "${LIFERAY_VERSION}" .appServerTomcatVersion)
-
 	BUILD_TIMESTAMP="${TIME_STAMP}"
 
 	BUNDLE_URL=$(get_yml "${main_key}" "${LIFERAY_VERSION}" bundle_url)
+
+	TOMCAT_VERSION=$(get_json "${LIFERAY_VERSION}" .appServerTomcatVersion)
 
 	LIFERAY_DOCKER_FIX_PACK_URL=$(get_yml "${main_key}" "${LIFERAY_VERSION}" fix_pack_url)
 
@@ -106,13 +138,13 @@ function set_value {
 
 function main {
 	get_time_stamp "${1}"
-	give_version_format
+
 	if [[ $(grep -c "${TIME_STAMP}" bundles.yml) -gt 0 ]]
 	then
 		echo "${TIME_STAMP}"
 		echo "It's in bundle.yml"
-		#set_value "${1}"
-		#generate_release_properties_file
+		set_value "${1}"
+		generate_release_properties_file
 	else
 		echo "${TIME_STAMP}"
 		echo "It's not in there"
